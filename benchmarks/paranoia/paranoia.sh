3bc881cb3475e79ff1411f0d356ec13ced322e57
tdh0ang <td.hoang03@gmail.com>
2025-01-28 14:14:09 +0100

    Adjusted bash script to use the cross compiler

--- benchmarks/paranoia/paranoia.sh
+++ benchmarks/paranoia/paranoia.sh
@@ -1,18 +1,20 @@
 #! /bin/bash
-#
-gcc -c -Wall paranoia.c
+
+CC="../../toolchain/toolchain/bin/powerpc-buildroot-linux-uclibcspe-gcc"
+FLAGS="-D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -mabi=spe -mfloat-gprs=double -Wa,-me500x2 -O2 -g0"
+
+$CC -c -Wall paranoia.c
 if [ $? -ne 0 ]; then
   echo "Compile error."
   exit
 fi
-#
-gcc paranoia.o -lm
+
+$CC $FLAGS paranoia.o -lm -o paranoia
 if [ $? -ne 0 ]; then
   echo "Load error."
   exit
 fi
-#
+
 rm paranoia.o
-mv a.out ~/binc/paranoia
-#
+
 echo "Normal end of execution."
